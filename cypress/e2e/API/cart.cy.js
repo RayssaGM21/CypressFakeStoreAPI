e4364260ffe5d76fddf85b8f291632fe54a79378
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-09-26 20:39:32 -0300

    fix: corrigindo sintaxe

--- cypress/e2e/API/cart.cy.js
+++ cypress/e2e/API/cart.cy.js
@@ -1,21 +1,21 @@
 // 16
 describe('Funcionalidade: Cart API', () => {
-    it('Dado que quero listar todos os carrinhos, Quando der GET/Start, Então deve retornar 200 e um array'), () => {
+    it('Dado que quero listar todos os carrinhos, Quando der GET/Start, Então deve retornar 200 e um array', () => {
         cy.request("GET", "https://fakestoreapi.com/carts").then((response) => {
             expect(response.status).to.equal(200);
-            expect(response.status).to.be.an('array');
+            expect(response.body).to.be.an('array');
         })
-    }
+    })
 
-    it('Dado que tenho um carrinho válido, quando eu filtrar um id específico então eu recebo'), () => {
+    it('Dado que tenho um carrinho válido, quando eu filtrar um id específico então eu recebo', () => {
         cy.request("GET", "https://fakestoreapi.com/carts/1").then((response) => {
             expect(response.status).to.equal(200);
-            expect(response.status).to.have.property('useId', 1);
-            expect(response.status).to.have.property('id', 1);
-            expect(response.status).to.have.property('produtos').and.to.be.an('array');
+            expect(response.body).to.have.property('id', 1);
+            expect(response.body).to.have.property('userId', 1);
+            expect(response.body).to.have.property('products').and.to.be.an('array');
         })
-    }
-    it('Dado que desejo cadastrar um novo carrinho, quando eu enviar um payload válido, Então o carrinho será adicionado'), () => {
+    })
+    it('Dado que desejo cadastrar um novo carrinho, quando eu enviar um payload válido, Então o carrinho será adicionado', () => {
         const newCart = {
             id: 0,
             userId: 1,
@@ -31,6 +31,6 @@ describe('Funcionalidade: Cart API', () => {
             expect(response.body).to.have.property('userId', newCart.userId);
             expect(response.body).to.have.property('products').and.to.be.an('array');
         })
-    }
+    })
 });
 
